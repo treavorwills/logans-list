361ca8b49ecdb027e759228af6a92608f3fbbfa5
Combatxd <logan.schoerner@gmail.com>
2022-09-22 20:05:31 -0600

    lastroute

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const { Listing, User } = require('../models')
+const withAuth = require('../utils/auth');
 
+//Render the homepage with listings
 router.get('/', async (req, res) => {
     try {
         const listingData = await Listing.findAll({
-            include: [{model: User, attributes: ['name']}]
+            include: [{ model: User, attributes: ['name'] }]
         });
-        const listings = listingData.map((listing) => listing.get({plain: true}));
+        const listings = listingData.map((listing) => listing.get({ plain: true }));
 
         res.render('homepage', {
             listings,
@@ -14,7 +16,53 @@ router.get('/', async (req, res) => {
         });
     } catch (err) {
         res.status(500).json(err);
-      }
+    }
 });
+
+//Render page for the listing you choose 
+router.get('/listing/:id', async (req, res) => {
+    try {
+        const listingData = await Listing.findByPk(req.params.id, {
+            include: [{ model: User, attributes: ['name'] }]
+        });
+        const listing = listingData.get({ plain: true })
+
+        res.render('listing', {
+            ...listing,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//Render the profile page with listings
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Listing }],
+        });
+        const user = userData.get({ plain: true });
+
+        res.render('userprofile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//redirect login from withAuth if theyre not logged in 
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
