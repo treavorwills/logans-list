7d543b2c6db287e0f055d8fee508c7c429088e48
Combatxd <logan.schoerner@gmail.com>
2022-09-26 19:54:23 -0600

    update

---
+++ controllers/api/uploadRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Image } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newImage = await Image.create(req.body);
+        res.status(200).json(newImage);
+    } catch {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
---
+++ models/Image.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Image extends Model { }
+
+Image.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING
+        },
+        url: {
+            type: DataTypes.STRING
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'image',
+    }
+);
+
+module.exports = Image;
